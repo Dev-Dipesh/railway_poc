3a77b5440f4cedce12f650d1cd92d3dd387dc022
Dipesh Bhardwaj <itdeveloper03@gmail.com>
2023-09-19 19:23:44 +0700

    Testing on Public URL

--- internal_service/index.ts
+++ internal_service/index.ts
@@ -1,7 +1,7 @@
 console.log("Internal Service Started on - :: 3000");
 Bun.serve({
   port: 3000, // defaults to $BUN_PORT, $PORT, $NODE_PORT otherwise 3000
-  hostname: '::', // defaults to "0.0.0.0"
+  // hostname: '::', // defaults to "0.0.0.0"
   fetch(req) {
     return new Response("Hello Bun :)");
   },
